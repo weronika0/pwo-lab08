b4baa2dc30a8861249e4f3bbfb19590ccfb1dbab
Weronika <weronika.nowak@pollub.edu.pl>
2022-11-30 11:48:45 +0100

    Lab 8

---
+++ lab08-cli/src/main/java/pwo/lab08/cli/Lab08Cli.java
@@ -0,0 +1,12 @@
+package pwo.lab08.cli;
+
+import pwo.lab08.engine.Engine;
+
+public class Lab08Cli {
+    
+    public static final String APP_NAME = "Lab08 CLI Application ";
+
+    public static void main(String[] args) {
+        System.out.println(APP_NAME + Engine.getVersion() );
+    }
+}
---
+++ lab08-engine/src/main/java/pwo/lab08/engine/Engine.java
@@ -0,0 +1,11 @@
+package pwo.lab08.engine;
+
+public class Engine {
+    
+    public static final String
+     VERSION = "Number sequences engine version 1.0.0";
+
+    public static String getVersion() {
+        return VERSION;
+    }
+}
